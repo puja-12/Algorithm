f8a63e5309fdde0c807998cdc8e2d337ff8d6780
puja <pujarana1796@gmail.com>
2022-05-14 17:14:41 +0530

    puja added first commit

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -11,15 +11,37 @@ namespace Algorithm
     {
         public static void Main()
         {
-            String[] arr = { "India", "I", "from", "am" };
-            Console.WriteLine("unsorted string ");
+            while (true)
+            {
+                Console.WriteLine("select no\n1)insertion sort\n2)Bubble sort\n");
 
-            int n = arr.Length;
-            Console.WriteLine(String.Join(" ", arr));
-            InsertionSort ob = new InsertionSort();
-            Console.WriteLine("Sorted string");
-            ob.sort(arr, n);
-            ob.printArraystring(arr, n);
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+
+                        String[] arr = { "India", "I", "from", "am" };
+                        Console.WriteLine("unsorted string ");
+
+                        int n = arr.Length;
+                        Console.WriteLine(String.Join(" ", arr));
+                        InsertionSort ob = new InsertionSort();
+                        Console.WriteLine("Sorted string");
+                        ob.sort(arr, n);
+                        ob.printArraystring(arr, n);
+                        break;
+                    case 2:
+                        BubbleSort ob1 = new BubbleSort();
+                        int[] arr1 = { 64, 34, 25, 12, 22, 11, 90 };
+                        Console.WriteLine("unsorted values ");
+                        Console.WriteLine(String.Join(" ", arr1));
+                        ob1.bubbleSort(arr1);
+
+                        Console.WriteLine("Sorted values");
+                        ob1.printArray(arr1);
+                        break;
+                }
+            }
         }
     }
 }
