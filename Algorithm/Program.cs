027ff5391682078fe9cb2efdee6d2650f7ca6917
puja <pujarana1796@gmail.com>
2022-05-14 17:03:09 +0530

    puja added first commit

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to algorithm programming");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    class program
+    {
+        public static void Main()
+        {
+            String[] arr = { "India", "I", "from", "am" };
+            Console.WriteLine("unsorted string ");
+
+            int n = arr.Length;
+            Console.WriteLine(String.Join(" ", arr));
+            InsertionSort ob = new InsertionSort();
+            Console.WriteLine("Sorted string");
+            ob.sort(arr, n);
+            ob.printArraystring(arr, n);
+        }
+    }
+}
